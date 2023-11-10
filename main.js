c5bd8558b1a1552866e5db2e0a79a44cba510381
BurakTatli <135147407+BurakTatli@users.noreply.github.com>
2023-11-10 21:59:03 +0300

    main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const taskName = document.querySelector("#taskName")
 let gorevListesi = [
 //   { id: 1, gorevAdi: "1. Görev","durum":"tamamlanmis" },
 //   { id: 2, gorevAdi: "2. Görev","durum":"bekliyor" },
-];
+]
 
 if (localStorage.getItem("gorevListesi") !== null) {
     gorevListesi = JSON.parse(localStorage.getItem("gorevListesi"))
